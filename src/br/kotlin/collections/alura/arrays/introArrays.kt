171a66a19bc90d6e10689ea9507aaf6ef1547612
Lincoln Gadea <lincoln@glinc.com.br>
2021-04-13 17:50:07 -0300

    chore: cria iterador de array usando o índice

--- src/br/kotlin/collections/alura/arrays/introArrays.kt
+++ src/br/kotlin/collections/alura/arrays/introArrays.kt
@@ -14,15 +14,28 @@ fun main(){
     println(maiorIdade)
 
     //Iterando o array com foreach
-
     var menorIdade = Int.MAX_VALUE
     idades.forEach { idade ->
         if(idade < menorIdade){
             menorIdade = idade
         }
     }
-
     println(menorIdade)
+
+    //Iterando o array utilizando os seus índices
+    val salarios: DoubleArray = doubleArrayOf(1500.0, 2300.0, 3500.0, 5000.0, 5500.0, 7000.0)
+
+    val aumento = 1.1
+    for(indice in salarios.indices){
+        salarios[indice] = salarios[indice] * aumento
+    }
+    println(salarios.contentToString())
+
+    //Iterando o array utilizando o índice com forEachIndexed
+    salarios.forEachIndexed{i,salario->
+        salarios[i] = salario * aumento
+    }
+    println(salarios.contentToString())
 
 
 }
