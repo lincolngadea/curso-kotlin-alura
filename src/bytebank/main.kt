819b1fd1974097c213dc04e3f64b9a011793bd20
Lincoln Gadea <lincoln@glinc.com.br>
2021-04-13 09:57:20 -0300

    chore: trabalhando com exceptions

---
+++ src/bytebank/main.kt
@@ -0,0 +1,27 @@
+package br.com.alura.bytebank
+
+import br.com.alura.bytebank.modelo.Endereco
+
+fun main() {
+    println("início main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1(){
+    println("início funcao1")
+    try {
+        funcao2()
+    }catch (e: )
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5){
+        println(i)
+        val endereco = Any()
+        endereco as Endereco
+    }
+    println("fim funcao2")
+}
