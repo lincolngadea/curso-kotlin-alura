3c67c57e3ace02588b1d8c62fcf788ee3710094d
Lincoln Gadea <lincoln@glinc.com.br>
2021-04-16 10:50:20 -0300

    chore: apresenta exemplos de getValue, getOrElse e getOrDefault

---
+++ src/br/kotlin/collections/alura/exemplos/getOrElse.kt
@@ -0,0 +1,15 @@
+package br.kotlin.collections.alura.exemplos
+
+fun main(){
+
+    val pedidos: MutableMap<Int, Double?> = mutableMap()
+
+    val valorInformado = pedidos.getOrElse(5) { "Valor nulo" }
+
+    println(valorInformado)
+
+    /**
+     * Utilizando o getOrElse eu posso definir um valor para chaves inexistente ou nula
+     */
+
+}
--- src/br/kotlin/collections/alura/exemplos/main.kt
+++ src/br/kotlin/collections/alura/exemplos/main.kt
@@ -1,15 +1,16 @@
 package br.kotlin.collections.alura.exemplos
 
 fun main(){
-    val pedidos: Map<Int, Double> = mapOf(Pair(1,30.0), Pair(2,20.0), 3 to 50.0)
+    val pedidos: MutableMap<Int, Double?> = mutableMap()
+
     println(pedidos)
 
-//    val pedido:Double? = pedidos[1]
-//    pedido?.let { println("Pedido $it") }
+    val pedido:Double? = pedidos[1]
+    pedido?.let { println("Pedido $it") }
 
-    for(pedido: Map.Entry<Int,Double> in pedidos){
-        println("Número do pedido: ${pedido.key}")
-        println("Valor do pedido: ${pedido.value}")
+    for(p: Map.Entry<Int,Double?> in pedidos){
+        println("Número do pedido: ${p.key}")
+        println("Valor do pedido: ${p.value}")
         println()
     }
 }
