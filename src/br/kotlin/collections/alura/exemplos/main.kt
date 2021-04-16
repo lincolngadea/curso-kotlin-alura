c3049c9c70d3502b4177ea1184f1ad26c544cf2f
Lincoln Gadea <lincoln@glinc.com.br>
2021-04-16 08:59:31 -0300

    chore: cria exemplo de criação e leitura de estrutura Map

--- src/br/kotlin/collections/alura/exemplos/main.kt
+++ src/br/kotlin/collections/alura/exemplos/main.kt
@@ -1,10 +1,16 @@
 package br.kotlin.collections.alura.exemplos
 
 fun main(){
-    val alunosDeAndroid: MutableSet<String> = mutableSetOf("Rafael", "Pedro", "Lincoln", "Romário", "Carol")
-    val alunosDeKotlin: MutableSet<String> = mutableSetOf("Carol", "Lincolin", "Marílha", "Ricardo")
-    val todosOsAlunos:MutableSet<String> = mutableSetOf<String>()
-    todosOsAlunos.addAll(alunosDeAndroid)
-    todosOsAlunos.addAll(alunosDeKotlin)
-    println(todosOsAlunos)
+    val pedidos: Map<Int, Double> = mapOf(Pair(1,30.0), Pair(2,20.0), 3 to 50.0)
+    println(pedidos)
+
+//    val pedido:Double? = pedidos[1]
+//    pedido?.let { println("Pedido $it") }
+
+    for(pedido: Map.Entry<Int,Double> in pedidos){
+        println("Número do pedido: ${pedido.key}")
+        println("Valor do pedido: ${pedido.value}")
+        println()
+    }
 }
+
