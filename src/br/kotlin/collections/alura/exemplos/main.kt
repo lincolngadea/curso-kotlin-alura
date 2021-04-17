f428b6952df86dacdc5ae5ae294b07f8800a7d89
Lincoln Gadea <lincoln@glinc.com.br>
2021-04-17 10:20:01 -0300

    chore: finaliza curso 06

--- src/br/kotlin/collections/alura/exemplos/main.kt
+++ src/br/kotlin/collections/alura/exemplos/main.kt
@@ -2,6 +2,24 @@ package br.kotlin.collections.alura.exemplos
 
 fun main(){
 
-}
+    val parentesEFamiliares: List<String> = listOf(
+        "Rafael",
+        "Pedro",
+        "Lincoln",
+        "Jackson",
+        "Christiny",
+        "Jackeline",
+        "Eugrácia",
+        "Ananias",
+        "Sara",
+        "Rodrigo"
+    )
 
+    val agenda: Map<Char, List<String>> = parentesEFamiliares.groupBy { familia ->
+        familia.first()
+    }
+
+    println(agenda)
+
+}
 
